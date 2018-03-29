a0a5ed71b89b87d602521e8d099b8636b9dd4a8a
Nikolay Snegurskiy <snenn9@gmail.com>
2018-03-28 22:10:31 -0400

    add dao

--- src/main/java/com/showCars/dao/DBConnection.java
+++ src/main/java/com/showCars/dao/DBConnection.java
@@ -1,6 +1,5 @@
 package com.showCars.dao;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,18 +7,11 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL_DB =
-            "";
-
     public static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("jdbc:mysql://us-cdbr-iron-east-05.cleardb.net:3306/heroku_555437bd0a94962?reconnect=true"));
-
-        String username = "bdee5f193ec11c";
-        String password = "ac321792";
 
         return DriverManager.getConnection(
-                String.valueOf(dbUri),
-                username, password);
+                "jdbc:mysql://us-cdbr-iron-east-05.cleardb.net:3306/heroku_555437bd0a94962?reconnect=true",
+                "bdee5f193ec11c", "ac321792");
     }
 
 
