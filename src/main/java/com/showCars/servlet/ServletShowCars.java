beeebd9cfaf56c8a6dac165275a8796ae9b0b04d
Nikolay Snegurskiy <snenn9@gmail.com>
2018-04-04 10:23:43 -0400

    add dao

--- src/main/java/com/showCars/servlet/ServletShowCars.java
+++ src/main/java/com/showCars/servlet/ServletShowCars.java
@@ -1,6 +1,10 @@
 package com.showCars.servlet;
 
 
+import com.showCars.dao.IAdDao;
+import com.showCars.pojos.Ad;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(
         name = "MyServlet",
@@ -15,13 +20,20 @@ import java.io.IOException;
 )
 public class ServletShowCars extends HttpServlet {
 
+    @Autowired
+    private IAdDao adDao;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        List<Ad> ads = adDao.getAll();
+        System.out.println(ads);
         ServletOutputStream out = resp.getOutputStream();
         out.write("hello heroku".getBytes());
+        out.println(String.valueOf(ads));
         out.flush();
         out.close();
     }
+
 
 }
